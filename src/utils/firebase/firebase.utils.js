c673671da840b4b77ce5d5e8a709dcabc469ba67
Mindaugas Ulonas <mindegi15@gmail.com>
2023-08-23 16:15:12 +0300

    adding Firebase auth

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCat_f0ipzElU1O94nt6L0sGoDrYSRX_C8",
+    authDomain: "crwn-clothing3-db.firebaseapp.com",
+    projectId: "crwn-clothing3-db",
+    storageBucket: "crwn-clothing3-db.appspot.com",
+    messagingSenderId: "347124564511",
+    appId: "1:347124564511:web:6dd5a74fa79f17440b4b2a"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: 'select_account'
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
