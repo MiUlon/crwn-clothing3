52bea0939b2ad96c85ceb427c908c54758912572
MiUlon <mindegi@gmail.com>
2023-10-12 20:17:31 +0300

    added CartIcon and CartDropdown components

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,6 +3,8 @@ import { Fragment, useContext } from "react";
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CardDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import './navigation.styles.scss';
 
 const Navigation = () => {
@@ -25,7 +27,9 @@ const Navigation = () => {
                             SIGN IN
                         </Link>
                     )}
+                    <CartIcon />
                 </div>
+                <CardDropdown />
             </div>
             <Outlet />
         </Fragment>
