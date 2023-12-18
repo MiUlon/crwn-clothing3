2bb70de5db536a884bafe2f9475c10520eb7230b
MiUlon <mindegi@gmail.com>
2023-12-18 14:04:55 +0200

    implementing Styled-Components functionality for Button component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,5 @@
 import './cart-dropdown.styles.scss';
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASSES} from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
@@ -17,7 +17,7 @@ const CardDropdown = () => {
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>{cartItems.map((item) => <CartItem key={item.id} cartItem={item}/>)}</div>
-            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+            <Button onClick={goToCheckoutHandler} buttonType={BUTTON_TYPE_CLASSES.inverted}>GO TO CHECKOUT</Button>
         </div>
     );
 };
